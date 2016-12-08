decc5c2d7e0fb3b5a75ff72a8976a150d9484869
jt328mth <jt328mth@gmail.com>
2016-12-08 16:02:13 -0500

    added Name to the user database (typically only allows email, password in default)

--- app/src/main/java/com/jt328mth/yelpsimilar/Profile.java
+++ app/src/main/java/com/jt328mth/yelpsimilar/Profile.java
@@ -2,14 +2,23 @@ package com.jt328mth.yelpsimilar;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Profile extends Activity implements View.OnClickListener {
     private Button buttonUpdate;
     private EditText editTextBirthday;
     private EditText editTextGender;
+    private TextView textViewName;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +28,42 @@ public class Profile extends Activity implements View.OnClickListener {
         buttonUpdate= (Button) findViewById(R.id.buttonUpdate);
         editTextBirthday = (EditText) findViewById(R.id.editTextBirthday);
         editTextGender= (EditText) findViewById(R.id.editTextGender);
+        textViewName = (TextView) findViewById(R.id.textViewName);
+
 
         buttonUpdate.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    //Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    Toast.makeText(Profile.this, "User logged in: " + user.getEmail(), Toast.LENGTH_SHORT).show();
+                } else {
+                    // User is signed out
+                    //Log.d(TAG, "onAuthStateChanged:signed_out");
+                    Toast.makeText(Profile.this, "User Signed Out", Toast.LENGTH_SHORT).show();
+                }
+                // ...
+            }
+        };
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     @Override
--- app/src/main/java/com/jt328mth/yelpsimilar/SignUp.java
+++ app/src/main/java/com/jt328mth/yelpsimilar/SignUp.java
@@ -1,10 +1,8 @@
 package com.jt328mth.yelpsimilar;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,16 +12,19 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import static android.content.ContentValues.TAG;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class SignUp extends Activity implements View.OnClickListener{
     private Button buttonSignUp;
-    private EditText editTextFirst;
-    private EditText editTextLast;
+    private EditText editTextName;
     private EditText editTextEmail;
     private EditText editTextPassword;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +32,45 @@ public class SignUp extends Activity implements View.OnClickListener{
         setContentView(R.layout.activity_sign_up);
 
         buttonSignUp= (Button) findViewById(R.id.buttonSignUp);
-        editTextFirst= (EditText) findViewById(R.id.editTextFirst);
-        editTextLast= (EditText) findViewById(R.id.editTextLast);
+        editTextName= (EditText) findViewById(R.id.editTextName);
         editTextEmail= (EditText) findViewById(R.id.editTextEmail);
         editTextPassword= (EditText) findViewById(R.id.editTextPassword);
 
+
         buttonSignUp.setOnClickListener(this);
 
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    //Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    Toast.makeText(SignUp.this, "User logged in: " + user.getEmail(), Toast.LENGTH_SHORT).show();
+                } else {
+                    // User is signed out
+                    //Log.d(TAG, "onAuthStateChanged:signed_out");
+                    Toast.makeText(SignUp.this, "User Signed Out", Toast.LENGTH_SHORT).show();
+                }
+                // ...
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     @Override
@@ -45,6 +78,8 @@ public class SignUp extends Activity implements View.OnClickListener{
         //handles click events
         String email = editTextEmail.getText().toString();
         String password = editTextPassword.getText().toString();
+        //String name = editTextFirst.getText().toString() + " " + editTextLast.getText().toString();
+
         if (v == buttonSignUp) {
             Toast.makeText(this, "registering: " + email + password, Toast.LENGTH_SHORT).show();
             createAccount(email, password);
@@ -57,16 +92,22 @@ public class SignUp extends Activity implements View.OnClickListener{
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
-
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
+                        if (task.isSuccessful()) {
+                            FirebaseDatabase db = FirebaseDatabase.getInstance();
+                            // Email Set
+                            DatabaseReference ref = db.getReference("users").child(mAuth.getCurrentUser().getUid()).child("email");
+                            ref.setValue(mAuth.getCurrentUser().getEmail());
+                            // Name Set
+                            DatabaseReference refname = db.getReference("users").child(mAuth.getCurrentUser().getUid()).child("name");
+                            refname.setValue(editTextName.getText().toString());
+                        }
                         if (!task.isSuccessful()) {
-                            Toast.makeText(SignUp.this, "Authentication failed." + task.getException(),
+                            Toast.makeText(SignUp.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
     }
+
+
 }
